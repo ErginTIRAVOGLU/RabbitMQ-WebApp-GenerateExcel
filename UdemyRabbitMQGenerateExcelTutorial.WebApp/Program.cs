a4b5d72145cd4b948764fe30d9878eff5cc54f5b
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-03 22:00:04 +0300

    first commit

---
+++ UdemyRabbitMQGenerateExcelTutorial.WebApp/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQGenerateExcelTutorial.WebApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using(var scope = app.Services.CreateScope())
+{
+    var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    appDbContext.Database.Migrate();
+
+    if(!appDbContext.Users.Any())
+    {
+        userManager.CreateAsync(new IdentityUser() { UserName = "deneme", Email = "deneme@msn.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new IdentityUser() { UserName = "deneme2", Email = "deneme2@msn.com" }, "Password12*").Wait();
+    }
+}
+
+app.Run();
