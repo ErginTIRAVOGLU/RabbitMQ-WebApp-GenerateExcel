994996603131a15d2cce5a5c554267e0fcd0fce5
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-04 00:41:49 +0300

    CreateProductExcel ve Files methodları oluşturuldu.

--- UdemyRabbitMQGenerateExcelTutorial.WebApp/Controllers/ProductController.cs
+++ UdemyRabbitMQGenerateExcelTutorial.WebApp/Controllers/ProductController.cs
@@ -1,14 +1,56 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQGenerateExcelTutorial.WebApp.Models;
 
 namespace UdemyRabbitMQGenerateExcelTutorial.WebApp.Controllers
 {
     [Authorize]
     public class ProductController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ProductController(AppDbContext appDbContext, UserManager<IdentityUser> userManager)
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var fileName = $"product-excel-{Guid.NewGuid().ToString().Substring(1, 10)}";
+
+            UserFile userFile = new()
+            {
+                UserId = user.Id,
+                FileName = fileName,
+                FileStatus = FileStatus.Creating
+            };
+
+            await _appDbContext.AddAsync(userFile);
+            
+            await _appDbContext.SaveChangesAsync();
+            //TODO: RabbitMQ mesaj gönder
+            TempData["StartCreatingExcel"] = true;
+
+            return RedirectToAction(nameof(Files));
+        }
+
+        public async Task<IActionResult> Files()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+
+            return View(await _appDbContext.UserFiles.Where(x => x.UserId == user.Id).ToListAsync());
         }
     }
 }
